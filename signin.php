b70dc8d310de4d5973524b804b13c0466ef82a84
Lutro <stevengamez00@gmail.com>
2020-11-06 12:51:23 -0800

    fixed links and fixed sign in page

--- signin.php
+++ signin.php
@@ -1,4 +1,9 @@
 <?php
 require_once 'scripts/helper.php';
 
-render_page("sign-in.twig",[]);
+render_page("sign-in.twig",["nav"=>navList(), 
+"address" =>address(), 
+"title"=>title(),
+"page_title"=>"SLogin", 
+"heading"=>"Management",
+"address" =>address()]);
