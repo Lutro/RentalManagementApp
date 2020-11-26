655f27f794f362c03057235cc1ab94990db5f744
Amanda <60409438+amandameganchan@users.noreply.github.com>
2020-11-26 02:52:58 -0800

    add inspections to maintenance tab

---
+++ list_inspections.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'scripts/helper.php';
+
+
+/*get the tenant information, to populate the tenant list from the database */
+$dbh = get_database_object();
+/* check connection */
+terminate_on_connect_error();
+
+$tenantQuery = $dbh->prepare('call getInspections()');
+$success = $tenantQuery->execute();
+
+terminate_on_query_error($success); //program will terminate on error
+
+$resultArray = $tenantQuery->get_result()->fetch_all(MYSQLI_ASSOC);
+
+
+$keys = ["Suite Number", "Inspection Date", "Reason", "Description"];
+
+$renderParams = ["nav"=>navList(), 
+                 "address" =>address(), 
+                 "title"=>title(),
+                 "page_title"=>"Inspections", 
+                 "heading"=>"Inspections",
+                 "table" => $resultArray,
+                 "keys" => $keys ];
+
+
+render_page("repair-orders-maint.twig", $renderParams);
+    // $data = [
+    //     ['Microsoft Internet Explorer', 56.33],
+    //     ['Chrome', 24.03],
+    //     ['Firefox', 10.38],
+    //     ['Safari', 4.77],
+    //     ['Opera', 0.91],
+    //     ['Proprietary or Undetectable', 0.2]
+    // ];
+
+    // $ob = new Highchart();
+    // $ob->chart->renderTo('container');
+    // $ob->chart->type('pie');
+    // $ob->title->text('My Pie Chart');
+    // $ob->series(array(array("data"=>$data)));
+
+    // render_page('piechart.twig', [
+    //     'mypiechart' => $ob
+    // ]);
