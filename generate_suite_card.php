2bce6afcbd3fc971ff9bf15e2a04b71b2e083d6a
Lutro <stevengamez00@gmail.com>
2020-11-25 21:43:39 -0800

    added borders to view suites

--- generate_suite_card.php
+++ generate_suite_card.php
@@ -17,28 +17,7 @@ $success = $filterQuery->execute();
 terminate_on_query_error($success); //program will terminate on error
 
 $resultArray = $filterQuery->get_result()->fetch_all(MYSQLI_BOTH);
-var_dump($resultArray);
 
-
-// if($success){                
-//     render_page("error.twig",array("message" => "Tenant with ID: ".$tenantID." deleted"));
-//     exit();
-// }   
-
-
-// $tenantQuery = $dbh->prepare('call getTenants()');
-// $success = $tenantQuery->execute();
-
-// terminate_on_query_error($success); //program will terminate on error
-
-// $resultArray = $tenantQuery->get_result()->fetch_all(MYSQLI_ASSOC);
-// $tenantQuery->close();
-
-
-// for($x=0; $x < count($resultArray); $x += 1){
-//     $resultArray[$x] += ["delete"=>"<a href='delete_tenant.php?id=".$resultArray[$x]["id"]."'>Delete</a>"];
-// }
-// $keys = ["name","phone","email","numberOfBikes","storageLockerNumber","numberOfPets", "leaseStart", "leaseEnd","delete"];
 
 $renderParams = ["nav"=>navList(), 
                  "address" =>address(), 
