dbb8b64284bde52d47c473a5396a936b99de41a9
Lutro <stevengamez00@gmail.com>
2020-11-25 20:56:07 -0800

    added activity log for tenant table

--- show_tenant_activity.php
+++ show_tenant_activity.php
@@ -11,3 +11,19 @@ $tenantQuery = $dbh->prepare('call getTenantActivityLog()');
 $success = $tenantQuery->execute();
 
 terminate_on_query_error($success); //program will terminate on error
+
+$resultArray = $tenantQuery->get_result()->fetch_all(MYSQLI_ASSOC);
+
+
+$keys = ["Action ID", "Action", "Action Time","Tenant ID","Lease Start","Lease End"];
+
+$renderParams = ["nav"=>navList(), 
+                 "address" =>address(), 
+                 "title"=>title(),
+                 "page_title"=>"Tenant Activity Log", 
+                 "heading"=>"Tenant Activity Log",
+                 "table" => $resultArray,
+                 "keys" => $keys ];
+
+
+render_page("repair-orders-stats.twig", $renderParams);
