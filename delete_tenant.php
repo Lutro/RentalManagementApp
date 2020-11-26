c2d4bd557ddc7dd74ef1af48c89e7033228b3b2f
Amanda <60409438+amandameganchan@users.noreply.github.com>
2020-11-26 02:46:42 -0800

    remove debug print statement

--- delete_tenant.php
+++ delete_tenant.php
@@ -7,7 +7,7 @@ $dbh = get_database_object();
 terminate_on_connect_error();
 
 $tenantID = (int)$_GET['id'];
-echo gettype($tenantID);
+//echo gettype($tenantID);
 
 $tenantIDQuery = $dbh->prepare('call removeTenant(?)');
 $tenantIDQuery->bind_param('i', $tenantID);
