aed6faacb7f5ce689835bb88ac2ffdc263a4532b
Lutro <stevengamez00@gmail.com>
2020-11-26 20:48:35 -0800

    manager profile pic and added nav var to delete operation

--- list_suites.php
+++ list_suites.php
@@ -6,23 +6,10 @@ $dbh = get_database_object();
 /* check connection */
 terminate_on_connect_error();
 
-// $options = ["bedrooms", "bathrooms"];
-// for ()
-// $options_sql = "SELECT DISTINCT " $option "FROM suite"
-
-
-// $filterQuery = $dbh->prepare('call getSuiteBedroomNumbers()');
-// $success = $filterQuery->execute();
-// terminate_on_query_error($success); //program will terminate on error
-// $bedroomsArray = $filterQuery->get_result()->fetch_all(MYSQLI_BOTH);
-
-// $filterQuery->close();
-
-
 // just stub values for pagination
 // calculate your own values
 $offset = 0;
-$limit = 10; 
+$limit = 25;
 
 // always initialize a variable before use!
 $conditions = [];
@@ -34,7 +21,7 @@ if (!empty($_GET['Bedrooms']))
     // here we are using LIKE with wildcard search
     // use it ONLY if really need it
     $conditions[] = 's.bedrooms = ?';
-    $parameters[] = '%'.$_GET['Bedrooms']."%";
+    $parameters[] = $_GET['Bedrooms'];
 }
 
 if (!empty(valueIfExists('Bathrooms', $_GET)))
@@ -80,11 +67,12 @@ $parameters[] = $limit;
 
 // the usual prepare/bind/execute/fetch routine
 $stmt = $dbh->prepare($sql);
-var_dump($sql);
+
 $stmt->bind_param(str_repeat("s", count($parameters)), ...$parameters);
 $stmt->execute();
 $resultArray = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-var_dump($resultArray);
+
+
 
 
 // $tenantQuery = $dbh->prepare('call getSuites()');
@@ -98,7 +86,7 @@ for($x=0; $x < count($resultArray); $x += 1){
     $resultArray[$x] += ["View Suite"=>"<a href='generate_suite_card.php?suiteNumber=".$resultArray[$x]["Suite Number"]."'>View</a>"];
 } 
 $keys = ["Suite Number","Bedrooms","Bathrooms","Rent Amount","Size", "View Suite"];
-$options = ["Bedrooms","Bathrooms","hasMasterKey"];
+$options = ["Bedrooms","Bathrooms"];
 $renderParams = ["nav"=>navList(), 
                  "address" =>address(), 
                  "title"=>title(),
