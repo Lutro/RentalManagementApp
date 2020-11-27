c229902a1d7a3f3b3dc77f6a08556b020581ce94
Amanda <60409438+amandameganchan@users.noreply.github.com>
2020-11-26 19:16:38 -0800

    add nested aggregation query

---
+++ avg_quote_per_suite.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'scripts/helper.php';
+
+
+/*get the information, to populate the list from the database */
+$dbh = get_database_object();
+/* check connection */
+terminate_on_connect_error();
+
+$tenantQuery = $dbh->prepare('call getSuiteAvgSpent()');
+$success = $tenantQuery->execute();
+
+terminate_on_query_error($success); //program will terminate on error
+
+$resultArray = $tenantQuery->get_result()->fetch_all(MYSQLI_ASSOC);
+
+
+$keys = ["Suite Number", "Average Spent ($)"];
+
+$renderParams = ["nav"=>navList(), 
+                 "address" =>address(), 
+                 "title"=>title(),
+                 "page_title"=>"Average Quote Per Suite", 
+                 "heading"=>"Average Quote Per Suite",
+                 "table" => $resultArray,
+                 "keys" => $keys ];
+
+
+render_page("repair-orders-stats.twig", $renderParams);
